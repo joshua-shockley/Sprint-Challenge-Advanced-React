8d502eb5544051e542c030558c6838064725ad5a
Joshua-Shockley <thelumbashockley@gmail.com>
2019-09-06 14:40:35 -0500

    got a hook.... welcome to purple mode...lol.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react';
-
+import {usePurpleMode} from './usePurpleMode.js';
 export default function Header() {
-const[darkMode, setDarkMode]= useState(false);
+const[purpleMode, setPurpleMode]= usePurpleMode(false);
 
 const theSwitch = e => {
     e.preventDefault();
-    setDarkMode(!darkMode);
-    if(darkMode === true){
+    setPurpleMode(!purpleMode);
+    if(purpleMode === true){
         document.body.classList.add("dark-mode");
     } else { document.body.classList.remove("dark-mode")};
 
@@ -18,7 +18,7 @@ const theSwitch = e => {
 //     } else { document.body.classList.remove("dark-mode")};
 // }, [darkMode]);
 
-console.log(darkMode);
+console.log(purpleMode);
     return(
         <header className="App-header" >    
             <h1 >Hello! Welcome To The Ladies Of Soccer</h1>
